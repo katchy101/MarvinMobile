e829fd1337fd34538891e196732a6e3bfd207ab8
unknown <alex.katz@ALEXKATZ.admach.co.za>
2014-11-20 13:37:30 +0200

    Boot + Login

--- js/app/views/pages/boot.js
+++ js/app/views/pages/boot.js
@@ -17,51 +17,57 @@ define([
       el: $('#appMiddle'),
 
       render: function () {
+
         var compiledTemplate = _.template(bootTemplate);
         this.$el.html(compiledTemplate);
-          $('.Collage').removeWhitespace().collagePlus();
-          setInterval(function(){
-            $('.Collage img').each(function(index,value){
+        $('.Collage').removeWhitespace().collagePlus();
+
+        $('.loader_cover').fadeIn(function(){
+          $('.login_area').fadeIn();
+        });
+
+        setInterval(function(){
+          $('.Collage img').each(function(index,value){
             var randomTag = Math.floor(Math.random() * 8) + 1
-              if(index == randomTag){
-                $(value).fadeOut(300, function(){
-                  var randomImage = Math.floor(Math.random() * 35) + 1
-                  $(this).attr('src','js/app/content/images/People/'+randomImage+'.jpg').bind('onreadystatechange load', function(){
-                     if (this.complete) $(this).fadeIn(300);
-                  });
-               });
-             }
-            })
-          },1800)
+            if(index == randomTag){
+              $(value).fadeOut(300, function(){
+                var randomImage = Math.floor(Math.random() * 35) + 1
+                $(this).attr('src','js/app/content/images/People/'+randomImage+'.jpg').bind('onreadystatechange load', function(){
+                  if (this.complete) $(this).fadeIn(300);
+                });
+              });
+            }
+          })
+        },1000)
 
-          //Loading Screen
-          //Phase 1 :
-          setTimeout(function(){
-            $('.logo_holder img').animate({
-              width:'21%',
-              height:'12%',
-            });
+        //Loading Screen
+        //Phase 1 :
+        // setTimeout(function(){
+        //   $('.login_area').fadeOut(function(){
+        //     $('.logo_holder img').animate({
+        //       width:'21%',
+        //       height:'12%',
+        //     });
+        //     $('.loader_cover').animate({
+        //       width:'48%'
+        //     })
+        //   });
+        // },1000)
+        //
+        // //Phase 2 :
+        // setTimeout(function(){
+        //      $('.loader_cover').animate({
+        //        width:'0%'
+        //      });
+        //      $('.logo_holder img').animate({
+        //        left:'0px'
+        //      });
+        // },2000)
 
-            $('.loader_cover').animate({
-              width:'48%'
-            })
-          },1000)
-
-          //Phase 2 :
-          setTimeout(function(){
-               $('.loader_cover').animate({
-                 width:'0%'
-               });
-               $('.logo_holder img').animate({
-                 left:'0px'
-               });
-          },2000)
-
-
-      },
-
-      initialize: function () {
-
+        //Phase 3 :
+        // setTimeout(function(){
+        //   $('.Collage').fadeOut();
+        // },5500)
       },
 
       close: function () {
